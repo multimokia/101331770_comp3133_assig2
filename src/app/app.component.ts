7c9ccbec90794143863b6eedb296cd46ea35cbc8
multimokia <multimokia3@gmail.com>
2023-03-16 19:11:01 -0400

    change wallpaper

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from "@angular/core";
 @Component({
     selector: "app-root",
     template: `
-        <div class="w-full h-full font-[GeosansLight] bg-[url('https://www.pixelstalk.net/wp-content/uploads/2016/06/Ice-HD-Picture.jpeg')]">
+        <div class="w-full h-full font-[GeosansLight] bg-[url('https://raw.githubusercontent.com/catppuccin/wallpapers/main/landscapes/Bridge.jpg')] bg-cover">
             <app-navbar/>
             <div class="flex flex-col items-center justify-center h-full">
                 <div class="w-fit text-white">
